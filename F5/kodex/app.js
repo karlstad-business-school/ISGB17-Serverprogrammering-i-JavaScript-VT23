772e0e036bbe7790c49f4308dcc292790b0715f8
Pierre Sjöberg <pierre.sjoberg@kau.se>
2023-05-11 11:49:07 +0200

    F5 slutkod

--- F5/kodex/app.js
+++ F5/kodex/app.js
@@ -4,3 +4,35 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+http.listen(3001, function(){
+    console.log('Tjo!');
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/favicon.ico', function(req,res) {
+    res.sendFile(__dirname + '/favicon.ico');
+});
+
+app.use('/banan', express.static(__dirname + '/clientscripts'));
+
+io.on('connection', (socket)=> {
+    console.log('Ny användare anslöt...');
+
+    socket.on('finafisken', (data)=> {
+        console.log(data);
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+
+        io.emit('newcolor', {'red' : r, 'green': g, 'blue': b});
+    });
+
+
+
+});
+
+
+
--- F5/kodex/clientscripts/clientscript.js
+++ F5/kodex/clientscripts/clientscript.js
@@ -1,1 +1,18 @@
 'use strict';
+const socket = io();
+
+window.addEventListener('load', ()=> {
+    document.querySelector('.btn').addEventListener('click', buttonClick);
+});
+
+
+function buttonClick(evt) {
+    socket.emit('finafisken','en massa data...');
+}
+
+socket.on('newcolor', (data)=> {
+    let body = document.querySelector('body');
+    body.setAttribute('style','background-color: rgb(' + data.red + ',' + data.green + ',' + data.blue + ')');
+     
+
+});
